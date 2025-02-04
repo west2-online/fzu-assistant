cbd850c142a3ef10fb9e3dade492299b2d592fbd
co-gy <gyc5529@gmail.com>
2025-02-05 03:40:07 +0800

    zhipu llm

--- demo3/llms.py
+++ demo3/llms.py
@@ -1,17 +1,18 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 from config import conf
 from langchain_mistralai import ChatMistralAI
+from langchain_community.chat_models import ChatZhipuAI
 
 
-llm = HuggingFacePipeline.from_model_id(
-    model_id=conf.llm,
-    task="text-generation",
-    pipeline_kwargs=dict(
-        max_new_tokens=2048,
-        repetition_penalty=1.03,
-    ),
-)
-llm = ChatHuggingFace(llm=llm)
+# llm = HuggingFacePipeline.from_model_id(
+#     model_id=conf.llm,
+#     task="text-generation",
+#     pipeline_kwargs=dict(
+#         max_new_tokens=2048,
+#         repetition_penalty=1.03,
+#     ),
+# )
+# llm = ChatHuggingFace(llm=llm)
 
 
 mistral = ChatMistralAI(
@@ -19,3 +20,12 @@ mistral = ChatMistralAI(
     temperature=0,
     max_retries=2
 )
+
+zhipu = ChatZhipuAI(
+    model="glm-4",
+    temperature=0.5,
+)
+
+
+if __name__ == "__main__":
+    print(zhipu.invoke("你好"))
