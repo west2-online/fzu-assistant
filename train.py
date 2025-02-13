200f44c30450b71ddc1d6a9be056a1241e04d1ab
co-gy <gyc5529@gmail.com>
2025-02-13 23:52:02 +0800

    add tolerant code for training

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from graph_store import GraphStore
 from vector_store import VectorStore
 from llms import tool_llm
 from embeddings import embeddings
+from utils import DataLoader
 from config import conf
 
 def train():
@@ -11,4 +12,19 @@ def train():
     loader = DataLoader(conf.data_dir)
     documents = loader.load_and_split()
     vector_store.add_documents(documents=documents)
-    graph_store.add_documents(documents=documents)
+    for batch_num, i in enumerate(range(0, len(documents), 10), 1):
+        chunk_documents = documents[i:i+10]
+        print(f"第 {batch_num} 批数据")
+        print("vector done")
+        try:
+            graph_store.add_documents(documents=chunk_documents)
+        except Exception as e:
+            try:
+                graph_store.add_documents(documents=chunk_documents)
+            except Exception as e:
+                continue
+        print("graph done")
+
+
+if __name__ == "__main__":
+    train()
