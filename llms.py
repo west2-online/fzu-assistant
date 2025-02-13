853db90a56634d308a4a04759c286bf0ca88b8d4
co-gy <gyc5529@gmail.com>
2025-02-13 23:49:58 +0800

    add vllm client

--- llms.py
+++ llms.py
@@ -7,9 +7,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 tool_llm = HuggingFacePipeline.from_model_id(
     model_id=conf.tool_llm,
     task="text-generation",
+    device=0,
     pipeline_kwargs=dict(
         max_new_tokens=4096,
-        repetition_penalty=1.03,
+        repetition_penalty=1.03
     ),
 )
 tool_llm = ChatHuggingFace(llm=tool_llm)
@@ -17,12 +18,23 @@ tool_llm = ChatHuggingFace(llm=tool_llm)
 chat_llm = HuggingFacePipeline.from_model_id(
     model_id=conf.chat_llm,
     task="text-generation",
+    device=1,
     pipeline_kwargs=dict(
         max_new_tokens=4096,
-        repetition_penalty=1.03,
+        repetition_penalty=1.03
     ),
 )
 chat_llm = ChatHuggingFace(llm=chat_llm)
+
+
+# from langchain_community.llms import VLLMOpenAI
+
+# llm = VLLMOpenAI(
+#     openai_api_key="EMPTY",
+#     openai_api_base="http://localhost:8000/v1",
+#     model_name="/home/xezx/AI_answer/demo3/models/DeepSeek-R1-Distill-Qwen-7B"
+# )
+
 
 def query_stream(llm, question: str):
   for chunk in llm.stream(question):
@@ -32,5 +44,6 @@ def query_stream(llm, question: str):
 
 if __name__ == "__main__":
     question = "你觉得人生是什么"
-    for answer in query_stream(tool_llm, question):
-        print(answer)
+    for answer in query_stream(llm, question):
+        print(answer, end="")
+    print(llm.invoke(question))
